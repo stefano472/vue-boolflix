c1b3ea7765b50ce24f0282cfe97d9b5452a8738e
stefano472 <stefanopiotti472@gmail.com>
2022-04-20 17:55:09 +0200

    header input searchbar + emit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,5 +11,14 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+  main  {
+    height: 100vh;
+    width: 100%;
+    overflow-y: auto;
+    background-image: url("https://assets.nflxext.com/ffe/siteui/vlv3/8459cea4-79ab-4f27-9ef0-a7c92a30a9bb/f4b0e424-f578-46d1-ac77-ba8f4763d581/IT-it-20220411-popsignuptwoweeks-perspective_alpha_website_large.jpg");
+    background-position: center;
+    background-size: cover;
+    background-blend-mode: darken;
+    background-color: rgba($color: #000000, $alpha: 0.7);
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// import font awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// add icon imported to our library
+library.add(faMagnifyingGlass)
+
+// how to tel Vue.js about our Font Awesome icon component.
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
